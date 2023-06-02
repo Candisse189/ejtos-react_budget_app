6485b7c336d6bc8de5206d4b93099ed68e52cd54
candisse189 <candisse189@icloud.com>
2023-06-02 02:38:34 -0400

    changes to budget for input-increment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,19 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
+{/*this is where you left off. we have assigned an input, if u want to revert back just delete input
+research needs to be done for where const=state needs to be placed. GN!*/}
 const Budget = () => {
     const { budget } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <input
+                type="number"
+                name="budget"
+                value={budget}
+                step={10}
+                onChange={(e) =>
+                setBudget(e.target.value)} />
         </div>
     );
 };
